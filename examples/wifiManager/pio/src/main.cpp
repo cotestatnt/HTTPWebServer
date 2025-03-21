acb9edea36420bee4e52cc428dfd0b74066315b0
Tolentino Cotesta <cotestatnt@yahoo.com>
2025-03-21 19:44:24 +0100

    ..

--- examples/wifiManager/pio/src/main.cpp
+++ examples/wifiManager/pio/src/main.cpp
@@ -3,30 +3,16 @@
 #include <WebServer.h>
 #include <Preferences.h>
 
-// #include "wifiManager.h"
-
 const char* ssidAP = "ARDUINO_UNOR4_AP";
-bool runCaptivePortal = false;
-
-typedef struct {
-  char ssid[32];
-  char pass[64];
-} wifiConfig_t;
 
 Preferences prefs;
 WebServer server(80);
+WiFiManager wifiManager(server);
 
-size_t saveWifiConfig(const char* ssid, const char* pass) {
-  wifiConfig_t wifiConfig;
-  strcpy(wifiConfig.ssid, ssid);
-  strcpy(wifiConfig.pass, pass);
-  return prefs.putBytes("wifi", &wifiConfig, sizeof(wifiConfig));
-}
-
-wifiConfig_t getWiFiConfig() {
-  wifiConfig_t wifiConfig;
-  prefs.getBytes("wifi", &wifiConfig, sizeof(wifiConfig));
-  return wifiConfig;
+WifiConfig_t getWiFiConfig() {
+  WifiConfig_t config;
+  prefs.getBytes("wifi", &config, sizeof(config));
+  return config;
 }
 
 void printWiFiInfo() {
@@ -45,7 +31,7 @@ void setup() {
     while (1) {};
   }
 
-  wifiConfig_t config = getWiFiConfig();
+  WifiConfig_t config = getWiFiConfig();
 
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
@@ -84,15 +70,17 @@ void setup() {
     printWiFiInfo();
   }
 
+  // Configure WiFiManager callback in order to save wifi credentials usings preferences
+  wifiManager.onConnected( [](WifiConfig_t& newConfig) {
+    prefs.putBytes("wifi", &newConfig, sizeof(newConfig));
+    Serial.print("WiFi credentials saved to preferences for SSID: ");
+    Serial.println(newConfig.ssid);
 
-  server.enableWifiManager();
-  server.onWifiConnected([](const char* ssid, const char* pass) {
     printWiFiInfo();
-    if (saveWifiConfig(ssid, pass)) {
-      Serial.print("WiFi credentials saved to preferences for SSID: ");
-      Serial.println(ssid);
-    }
   });
+
+  // Start the WiFiManager
+  wifiManager.begin();
   
   // Reindirizza a /wifi
   server.on("/", []() {
@@ -100,7 +88,7 @@ void setup() {
     server.send(302, "text/plain", "");
   });
 
-
+ 
   server.begin();
   Serial.println("HTTP server started");
 }
