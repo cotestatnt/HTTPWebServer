cdba3e2cca9d7e8d94f3768492b85bf0a37f10dd
Tolentino Cotesta <cotestatnt@yahoo.com>
2025-06-23 08:29:18 +0200

    network config

--- src/NetworkConfig.h
+++ src/NetworkConfig.h
@@ -6,14 +6,7 @@
 #define USING_WIFI      1
 #define USING_ETHERNET  2
 
-<<<<<<< Updated upstream
-// Uncomment if you need to enable support for ENC28J60 Ethernet driver
-// #define WEBSOCKETS_NETWORK_TYPE NETWORK_W5100
-
-#endif
-=======
 // Modify this value to enable Ethernet
 #define HARDWARE_TYPE USING_WIFI
 
 #endif
->>>>>>> Stashed changes
